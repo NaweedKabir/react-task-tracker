852bff8abd44a8c7ce9902d099ce5d20435f2208
NaweedKabir <acad.naweedkabir@gmail.com>
2022-02-05 21:43:21 +0600

    Initial Commit

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+const About = () => {
+  return (
+  <div>
+      <h4>Created by</h4>
+      <h4>Naweed Kabir</h4>
+      <Link to='/'>Go Back</Link>
+  </div>
+    )
+}
+
+export default About;
